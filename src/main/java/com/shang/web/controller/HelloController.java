daad4e4f1a17e0e5985009ecfbdd028820ef1d6e
ethan <905298857@qq.com>
2019-06-10 00:01:07 +0800

    加日志

--- src/main/java/com/shang/web/controller/HelloController.java
+++ src/main/java/com/shang/web/controller/HelloController.java
@@ -1,7 +1,18 @@
 package com.shang.web.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        log.info("测试一下日志info");
+        log.debug("测试一下日志debug");
+        log.error("测试一下日志error");
+        return "hello1";
+    }
 }
